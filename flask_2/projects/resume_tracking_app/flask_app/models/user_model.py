915f087cc1211f5d97d524d2349dae535718e035
reedba <72387245+reedba@users.noreply.github.com>
2022-05-20 10:40:21 -0400

    start of navigation

---
+++ flask_2/projects/resume_tracking_app/flask_app/models/user_model.py
@@ -0,0 +1,88 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+import re
+
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask import flash
+
+class User:
+    db = "resume_db"
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s);"
+        return connectToMySQL(cls.db).query_db(query,data)
+    
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(cls.db).query_db(query)
+        users = []
+        for row in results:
+            users.append(cls(row))
+        return users
+
+    @classmethod
+    def get_by_email(cls, data):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def get_reset_email(cls, data):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def get_by_id(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return cls(results[0])
+
+    @classmethod
+    def get_one(cls,id):
+        query  = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL(cls.db).query_db(query, {'id':id})
+        print(result, "$"*60)
+        return cls(result[0])
+
+    @staticmethod
+    def validate_register(user):
+        is_valid = True
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        results = connectToMySQL(User.db).query_db(query,user)
+        if len(results) >= 1:
+            flash("Email is already taken.","register")
+            is_vailid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid Email!!!", "register")
+            is_vailid = False
+        if len(user['first_name']):
+            flash("First name must be atleast 3 characters", "register")
+            is_vailid = False
+        if len(user["last_name"]) < 3:
+            flash("First name must be atleast 3 characters", "register")
+            is_valid = False
+        if len(user["password"]) < 8:
+            flash("Password must be atleast 8 characters", "register")
+            is_valid = False
+        if user["password"] != user["confirm_password"]:
+            flash("Passwords do not match", "register")
+            is_valid = False
+        return is_valid 
